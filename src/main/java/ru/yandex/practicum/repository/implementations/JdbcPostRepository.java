ffa9362b3dcb617d44e2e5d3cf48a8f4c195489c
Alex Tsarenkov <alexey.tsarenkov@yandex.ru>
2026-01-20 15:44:35 +0300

    Исправить поиск + правки checkstyle + правки схемы БД

--- src/main/java/ru/yandex/practicum/repository/implementations/JdbcPostRepository.java
+++ src/main/java/ru/yandex/practicum/repository/implementations/JdbcPostRepository.java
@@ -8,10 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.model.Post;
 import ru.yandex.practicum.repository.interfaces.PostRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Repository("JdbcPostRepository")
 @AllArgsConstructor
@@ -20,27 +17,33 @@ public class JdbcPostRepository implements PostRepository {
 
     @Override
     public List<Post> getPosts(String searchString, Integer pageSize, Integer currentPage) {
-        Map<String,Object> params = new HashMap<>();
-
+        Map<String, Object> params = new HashMap<>();
+        StringBuilder sql = new StringBuilder();
         //language=SQL
-        String query = "SELECT * FROM posts";
+        sql.append("""
+                SELECT ID,
+                       TITLE,
+                       CASE WHEN LENGTH(text) > 128
+                               THEN SUBSTRING(text FROM 1 FOR 128) || '...'
+                               ELSE text
+                           END AS text,
+                        likes_count as likesCount
+                FROM posts
+                """);
 
-        if (searchString != null && !searchString.isEmpty()) {
-            query += " WHERE search LIKE :searchString";
-            params.put("searchString", "%" + searchString + "%");
-        }
+        sql.append(buildSearchCause(searchString, params));
 
-        query += " ORDER BY id DESC LIMIT :pageSize";
+        sql.append(" ORDER BY id DESC LIMIT :pageSize");
 
         params.put("pageSize", pageSize);
 
         if (currentPage > 1) {
-            query += " OFFSET :os";
-            params.put("os", (currentPage - 1) * pageSize );
+            sql.append(" OFFSET :os");
+            params.put("os", (currentPage - 1) * pageSize);
         }
 
 
-        return jdbcTemplate.query(query, params, new BeanPropertyRowMapper<>(Post.class));
+        return jdbcTemplate.query(sql.toString(), params, new BeanPropertyRowMapper<>(Post.class));
     }
 
     @Override
@@ -48,11 +51,13 @@ public class JdbcPostRepository implements PostRepository {
         //language=SQL
         String sqlQuery = "SELECT count(*) FROM posts";
 
-        if (searchString != null && !searchString.isEmpty()) {
-            sqlQuery += " WHERE search LIKE %:searchString%";
-        }
+        Map<String, Object> params = new HashMap<>();
+        params.put("searchString", searchString);
 
-        return jdbcTemplate.queryForObject(sqlQuery, Map.of("searchString", searchString), Integer.class);
+        String searchCause = buildSearchCause(searchString, params);
+        sqlQuery += searchCause;
+
+        return jdbcTemplate.queryForObject(sqlQuery, params, Integer.class);
     }
 
     @Override
@@ -88,8 +93,8 @@ public class JdbcPostRepository implements PostRepository {
     public Post updatePost(Post post) {
         //language=SQL
         String sqlQuery = """
-                UPDATE posts 
-                SET title = :title, text = :text 
+                UPDATE posts
+                SET title = :title, text = :text
                 WHERE id = :id
                 """;
         Map<String, Object> params = Map.of(
@@ -115,5 +120,49 @@ public class JdbcPostRepository implements PostRepository {
         //language=SQL
         String sqlQuery = "UPDATE posts SET likes_count = likes_count + 1 WHERE id = :id";
         jdbcTemplate.update(sqlQuery, Map.of("id", id));
+    }
+
+    private String buildSearchCause(String searchString, Map<String, Object> params) {
+        StringBuilder searchStringBuilder = new StringBuilder();
+
+        if (searchString != null && !searchString.isEmpty()) {
+            searchStringBuilder.append(" WHERE");
+
+            String[] words = searchString.split(" ");
+            StringBuilder wordsSearch = new StringBuilder();
+            List<String> hashtags = new ArrayList<>();
+            for (String word : words) {
+                if (word.equals(" ") || word.equals("")) {
+                    continue;
+                }
+
+                if (word.startsWith("#")) {
+                    hashtags.add(word.substring(1));
+                } else {
+                    wordsSearch.append(" ");
+                    wordsSearch.append(word);
+                }
+            }
+            if (wordsSearch.length() > 0) {
+                wordsSearch.replace(0, 1, "%");
+                wordsSearch.append("%");
+
+                searchStringBuilder.append(" title LIKE :searchString");
+                params.put("searchString", wordsSearch.toString());
+            }
+
+            if (wordsSearch.length() > 0 && hashtags.size() > 0) {
+                searchStringBuilder.append(" AND");
+            }
+
+            if (hashtags.size() > 0) {
+                searchStringBuilder.append(" id IN (SELECT post_id FROM post_tags WHERE tag IN (:hashtags) )");
+                params.put("hashtags", hashtags);
+            }
+
+            return searchStringBuilder.toString();
+        } else {
+            return "";
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/repository/interfaces/TagRepository.java
+++ src/main/java/ru/yandex/practicum/repository/interfaces/TagRepository.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 public interface TagRepository {
     void saveTagsForPost(Long postId, List<String> tags);
+
     List<String> getTagsForPost(Long postId);
+
     Map<Long, Set<String>> getTagsForMultiplePostIds(List<Long> postIds);
 }
